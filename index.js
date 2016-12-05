06773996947490008a4db25d4b76454f1cddf312
Shirin Tag <taghavi.shirin@gmail.com>
2016-12-05 16:57:53 +0100

    missing edit post

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/register', function(req, res) {
               };
 
               if (req.session.user) {
-                  res.redirect('/petition');
+                  res.redirect('/register/info');
               }
           });
         }).catch(function(err){
@@ -70,7 +70,7 @@ app.get('/register/info', function(req, res) {
 app.post('/register/info', function(req, res) {
     db.query("INSERT INTO user_profiles(user_id, age, city, url) VALUES ($1, $2, $3, $4) RETURNING id",
     [req.session.user.id, req.body.age, req.body.city, req.body.url]).then(function(result){
-        res.redirect('/petition/profile');
+        res.redirect('/petition');
     }).catch(function(err){
         console.log(err);
     });
@@ -84,7 +84,7 @@ app.get('/login', function(req, res) {
 
 app.post('/login', function(req, res) {
     if (req.body.email && req.body.password){
-        db.query("SELECT users.first_name, users.last_name, users.id, petitioners.id as sign_id, password FROM users left join petitioners on petitioners.user_id = users.id WHERE email = $1",
+        db.query("SELECT users.first_name, users.last_name, users.id, petitioners.id as sign_id, password FROM users LEFT JOIN petitioners ON petitioners.user_id = users.id WHERE email = $1",
         [req.body.email]).then(function(result){
             checkPassword(req.body.password, result.rows[0].password).then(function(doesMatch){
                 if(doesMatch){
@@ -123,7 +123,6 @@ app.get('/petition', function(req, res) {
         res.redirect('/login');
         return;
     } else if (req.session.user.signId) {
-        console.log();
         res.redirect('/petition/profile');
         return;
     } else {
@@ -177,7 +176,6 @@ app.get('/petition/profile', function(req, res) {
         return db.query('SELECT * FROM petitioners WHERE id = $1', [req.session.user.signId]).then(function(results2) {
             // console.log(result.rows);
             // console.log(results2.rows);
-            console.log();
             res.render('profile', {
                 layout : 'layout',
                 signature: result.rows.length,
@@ -195,10 +193,23 @@ app.get('/petition/profile', function(req, res) {
 });
 
 app.get('/petition/profile/signatures', function(req, res) {
-    db.query("SELECT * FROM users").then(function(result){
+    db.query("SELECT * FROM petitioners LEFT JOIN user_profiles ON user_profiles.user_id = petitioners.user_id LEFT JOIN users ON users.id = petitioners.user_id").then(function(result){
+        console.log(result.rows);
         res.render('signature', {
             layout : 'layout',
             signatures: result.rows
+        });
+    });
+});
+
+app.get('/petition/profile/signatures/:city', function(req, res) {
+    var city = req.params.city;
+    db.query("SELECT * FROM petitioners LEFT JOIN user_profiles ON user_profiles.user_id = petitioners.user_id LEFT JOIN users ON users.id = petitioners.user_id WHERE city =$1 ", [city])
+    .then(function(result){
+        res.render('signature', {
+            layout : 'layout',
+            signatures: result.rows,
+            city : req.body.city
         });
     });
 });
@@ -209,6 +220,43 @@ app.get('/petition/error', function(req, res) {
         error: true
     });
 });
+
+app.get('/petition/profile/edit', function(req, res) {
+    db.query("SELECT * FROM users LEFT JOIN user_profiles ON users.id = user_profiles.user_id WHERE user_id = $1",
+    [req.session.user.id]).then(function(result){
+        console.log(result);
+        res.render('edit', {
+            layout : 'layout',
+            firstname: result.rows[0].first_name,
+            lastname: result.rows[0].last_name,
+            email: result.rows[0].email,
+            password: result.rows[0].password,
+            city: result.rows[0].city,
+            age: result.rows[0].age,
+            url: result.rows[0].url
+        });
+    }).catch(function(err){
+        console.log(err);
+    });
+});
+
+app.post('/petition/profile/edit', function(req, res) {
+    if (req.body.firstname || req.body.lastname || req.body.email || req.body.password || req.body.age || req.body.city || req.body.url ) {
+        hashPassword(req.body.password).then(function(hash){
+            return db.query("UPDATE users SET first_name = $1, last_name = $2, email = $3, password = $4 WHERE id = $5",
+            [req.body.firstname, req.body.lastname, req.body.email, hash, req.session.user.id]).then(function(resutl){
+                db.query("INSERT INTO user_profiles (age, city, url) VALUES ($1, $2, $3)", [req.body.age, req.body.city, req.body.url]).then(function(){
+
+                })
+
+            })
+        db.query("UPDATE * FROM users LEFT JOIN user_profiles ON users.id = user_profiles.user_id WHERE user_id = $1",
+        [req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.age, req.body.city, req.body.url, req.session.user]).then(function(resutl){
+
+        });
+    });
+    }
+}
 
 app.listen(8080, function(req, res) {
     console.log('!!!!');
